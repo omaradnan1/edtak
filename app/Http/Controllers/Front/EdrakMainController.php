eebd719810c9df29af70a96b40840a5fd072bdd2
omaradnan1 <102290406+omaradnan1@users.noreply.github.com>
2022-08-13 14:58:58 +0300

    first commit

---
+++ app/Http/Controllers/Front/EdrakMainController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class EdrakMainController extends Controller
+{
+   public function index()
+   {
+    return "hello"." "."adnan";
+
+   }
+
+
+   public function creat()
+   {
+
+   }
+
+
+   public function store()
+   {
+
+   }
+
+
+   public function update()
+   {
+
+   }
+
+   public function edit()
+   {
+
+   }
+
+   public function show()
+   {
+
+   }
+
+
+   public function delete()
+   {
+
+   }
+}
